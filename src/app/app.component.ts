25455ed009aea09eaa41ae4c0be4ee6d346f8b68
Matt David <mcdav3636@gmail.com>
2017-11-14 13:20:32 -0700

    add splash componenet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component} from "@angular/core";
 
 @Component({
-    selector: "angular5-example-app",
-    templateUrl: "./templates/angular5-example-app.html"
+    selector: "angular5-project-app",
+    templateUrl: "./templates/angular5-project-app.html"
 })
 
 export class AppComponent {}
